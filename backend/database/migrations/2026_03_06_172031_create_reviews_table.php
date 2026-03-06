63a5d02a4a16e27302b1b6db47dfee160c4fad50
pauline-tnd <230712297@students.uajy.ac.id>
2026-03-07 00:34:16 +0700

    add:migrations

---
+++ backend/database/migrations/2026_03_06_172031_create_reviews_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('room_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('booking_detail_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->tinyInteger('rating')->unsigned();
+
+            $table->string('description');
+            $table->string('image')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
