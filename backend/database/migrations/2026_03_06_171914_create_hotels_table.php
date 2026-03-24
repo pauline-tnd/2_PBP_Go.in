7805f48f6956b2bfbb37b990922ce1e5e4e92cd7
pauline-tnd <230712297@students.uajy.ac.id>
2026-03-24 23:00:03 +0700

    fix:migrations structure

--- backend/database/migrations/2026_03_06_171914_create_hotels_table.php
+++ backend/database/migrations/2026_03_06_171914_create_hotels_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
 
             $table->string('name');
+            $table->text('description')->nullable();
+
+            $table->tinyInteger('star')->unsigned();
             $table->string('location');
 
             $table->decimal('latitude');
--- backend/database/migrations/2026_03_23_124847_create_icons_table.php
+++ backend/database/migrations/2026_03_23_124847_create_icons_table.php
@@ -11,15 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('room_facilities', function (Blueprint $table) {
+        Schema::create('icons', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('room_id')
-                ->constrained()
-                ->cascadeOnDelete();
-            $table->foreignId('icon_id')
-                ->constrained()
-                ->cascadeOnDelete();
-
+            $table->string('icon');
             $table->string('name');
         });
     }
@@ -29,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('room_facilities');
+        Schema::dropIfExists('icons');
     }
 };
