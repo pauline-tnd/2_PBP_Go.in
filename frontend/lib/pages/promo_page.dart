bdc4713316ad846e358b9145608359c4a94474b2
pauline-tnd <230712297@students.uajy.ac.id>
2026-04-01 11:34:15 +0700

    update: frontend logo, navbar

---
+++ frontend/lib/pages/promo_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PromoPage extends StatelessWidget {
+  const PromoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(child: Text('Promo Page')),
+    );
+  }
+}
