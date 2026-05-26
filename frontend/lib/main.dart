d0542ff5b81f6a98436ce87d4f3829b7cfcc341c
SaskiaPippiRK <230712300@students.uajy.ac.id>
2026-05-26 12:23:51 +0700

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -9,8 +9,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load(fileName: ".env");
-
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
     anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
